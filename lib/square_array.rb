5f054d3aa6d1cd5d8068f3d8845fb0f436a425ea
Zeshan Raja <zraja4402@gmail.com>
2019-11-05 16:53:31 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,20 @@
 def square_array(array)
-index = 0
+counter = 0
+newArr = []
 
-while array[index] do
-puts array[index]
- 
-index = index ** 2
+while array[counter] do 
+  sq = array[counter] ** 2 
+  counter += 1 
+  newArr << sq
+end
+p newArr
 end 
 
+# counter = 0
+# pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
+ 
+# while pets[counter] do
+#   puts pets[counter]
+#   counter += 1
+# end
 
